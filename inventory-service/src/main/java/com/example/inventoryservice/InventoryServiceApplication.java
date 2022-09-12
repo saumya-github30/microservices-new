f5ec8ec8edf4e92fd160cae487473ef3aea87db8
anoop.khandelwal <2hN(,2YW-Ds2d'E>
2022-09-12 20:30:14 +0530

    Initial commit

---
+++ inventory-service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,37 @@
+package com.example.inventoryservice;
+
+import com.example.inventoryservice.model.Inventory;
+import com.example.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("product2");
+			inventory.setQuantity(20);
+
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("product1");
+			inventory1.setQuantity(0);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory1);
+		};
+	}
+
+}
---
+++ order-service/src/main/java/com/microservice/orderservice/service/OrderService.java
@@ -0,0 +1,65 @@
+package com.microservice.orderservice.service;
+
+import com.microservice.orderservice.dto.InventoryResponse;
+import com.microservice.orderservice.dto.OrderLineItemsDto;
+import com.microservice.orderservice.dto.OrderRequest;
+import com.microservice.orderservice.model.Order;
+import com.microservice.orderservice.model.OrderLineItems;
+import com.microservice.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final WebClient.Builder webClientBuilder;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        List<OrderLineItems> orderLineItemsList = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto).collect(Collectors.toList());
+        order.setOrderLineItemsList(orderLineItemsList);
+
+        List<String> skuCodes = order.getOrderLineItemsList().stream()
+                .map(OrderLineItems::getSkuCode).collect(Collectors.toList());
+
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponseArray)
+                .allMatch(InventoryResponse::isInStock);
+
+        if(allProductsInStock) {
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, please try later.");
+        }
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItems.getPrice());
+        orderLineItems.setQuantity(orderLineItems.getQuantity());
+        orderLineItems.setSkuCode(orderLineItems.getSkuCode());
+//        orderLineItems.setId(orderLineItems.getId());
+        return orderLineItems;
+    }
+
+
+}
